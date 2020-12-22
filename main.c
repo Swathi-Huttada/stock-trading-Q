af96dd7a45a0db7899effee2dff6218cdc83d968
Swathi-Huttada <52745868+Swathi-Huttada@users.noreply.github.com>
2020-12-22 11:05:57 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include " stack.h"
+
+q = queue_ad(q,10, &res);
+q = queue_ad(q,20, &res);
+assert(queue_length(q) == 3);
+assert(res.status == queue_fl);
+
+
+
+void test_Qdelete(){
+Queue q1 = queue_new(3);
+Queue +q =  &q1;
+}
